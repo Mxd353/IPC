674cdc0b868444017ca2ce21d1206f0919d73a12
Mxd353 <13154213167@163.com>
2024-03-15 18:09:41 +0800

    3.15

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QFileDialog>
+#include <QFile>
+#include <QMessageBox>
+#include <QGraphicsScene>
+#include <QPixmap>
+#include <QPointer>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +17,49 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString path ;
+    QPointer<QGraphicsScene> scene1 = new QGraphicsScene;
+
+    path = QFileDialog::getOpenFileName(this,"选择图片","../",tr("images(*.png *jpeg *bmp)"));
+    if(path.isEmpty()){
+        QMessageBox::warning(this, "Warning!", "Failed to open the image!");
+    }
+    else{
+        ui->lineEdit->setText(path);
+        ui->graphicsView->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+
+        scene1->addPixmap(QPixmap(path));
+        ui->graphicsView->setScene(scene1);
+        ui->graphicsView->show();
+        ui->graphicsView->fitInView(scene1->sceneRect(), Qt::KeepAspectRatio);
+
+    }
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString path;
+    QPointer<QGraphicsScene> scene2 = new QGraphicsScene;
+
+    path = QFileDialog::getOpenFileName(this,"选择图片","../",tr("images(*.png *jpeg *bmp)"));
+    if(path.isEmpty()){
+        QMessageBox::warning(this, "Warning!", "Failed to open the image!");
+    }
+    else{
+        ui->lineEdit_2->setText(path);
+        ui->graphicsView_2->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+
+        scene2->addPixmap(QPixmap(path));
+        ui->graphicsView_2->setScene(scene2);
+        ui->graphicsView_2->show();
+        ui->graphicsView_2->fitInView(scene2->sceneRect(), Qt::KeepAspectRatio);
+
+    }
 }
 
